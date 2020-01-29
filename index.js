4852955516a00941f8a4e036c1c5f47b4476cf90
Jeremy Kithome <jerekith@gmail.com>
2020-01-30 00:57:49 +0300

    fix: remove use of uninstalled dependency

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 const express = require("express");
-const errorHandler = require("errorhandler");
 const WebSocket = require("ws");
 const http = require("http");
 
 const app = express();
 
 const port = process.env.PORT || 9000;
-// Error handler
-if (process.env.NODE_ENV === "dev") {
-  // only use in development
-  app.use(errorHandler());
-}
 
 //initialize a http server
 const server = http.createServer(app);
